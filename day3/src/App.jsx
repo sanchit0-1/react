f6373dd7ab386f302e470e5c3327cfd0b85eb28d
Sanchit Gupta <sanc.aot24@gmail.com>
2025-07-20 09:47:41 +0530

    use of useState

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Fragment } from 'react';
+import { useState } from 'react'; // to use useState we have to import it from react
 
 // now here we will learn how to access arrays which has object in json format using map function 
 let array =[
@@ -23,19 +22,34 @@ let data = array.map((values, index) =>{
   return <h1 key={index}>{values.name} | {values.age}</h1> // here we had given key values which should be unique so that react can understand it well and able to make changes. otherwise it will through an array.
 })
 
+// From here we will learn how to use useState because if make changes by making ourseleves variable and making a function to click it will change the name , but it will not to make that change we have to learn useState (react hooks )
+
 const App = () => {
+  const [username, setusername] = useState("sanchit"); // remmber username is read only it cannot be changed in place of that we use setusername function to make changes by passing argument in it.
+
+  const handler = () =>{
+    setusername("anchit") // here how we have used stusername function and it is async code.
+
+  }
   return (
   <>
-    <h1>
-      object: {object.name} || {object.age}
-    </h1>
-
     {/* we are rendering data  */}
     <div>
       <ol>
         {data} /** here map func returns array now returned array is now in simpler form so it can render easily */
       </ol>  
     </div>  
+
+{/* using useState */}
+<h1>userName change onclick</h1>
+<h2>
+  {username}
+</h2>
+<button onClick={handler}>
+  change
+</button>
+
+  
   </>
   )
 }
